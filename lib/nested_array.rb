8d9d1860c8e7034ec0393f9d4cc3f79c1653f70a
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 16:18:30 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,25 +16,3 @@ CONVENTIONAL_PRODUCE = [
   "Eggplant"
 ]
 
-def assembled_matrix
-  return [
-    CONVENTIONAL_PRODUCE,
-    ORGANIC_PRODUCE
-    ]
-end
-
-def sorted_matrix
-  return [
-    CONVENTIONAL_PRODUCE.sort,
-    ORGANIC_PRODUCE.sort
-    ]
-end
-
-def matrix_lookup(matrix, row, column)
-  return matrix[row][column]
-end
-
-def matrix_update(matrix, row, column, new_value)
-  matrix[row][column] = new_value
-  return matrix
-end
